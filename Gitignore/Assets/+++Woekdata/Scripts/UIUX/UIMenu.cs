9083769961a04da8f50a3471c290688902a66e0a
motte041204@gmail.com <motte041204@gmail.com>
2024-06-17 15:33:42 +0200

    Tilemap and Level 4 done, Settings panel exists now

--- Gitignore/Assets/+++Woekdata/Scripts/UIUX/UIMenu.cs
+++ Gitignore/Assets/+++Woekdata/Scripts/UIUX/UIMenu.cs
@@ -9,12 +9,18 @@ public class UIMenu : MonoBehaviour
     [SerializeField] private CanvasGroup panelMain;
     [SerializeField] private Button buttonNewGame;
     [SerializeField] private Button buttonLevelSelection;
+    [SerializeField] private Button buttonSettings;
+    [SerializeField] private Button exitButton;
     
     [SerializeField] private CanvasGroup panelLevelSelection;
     [SerializeField] private Button buttonBackToMain;
     [SerializeField] private Button buttonLevel1;
     [SerializeField] private Button buttonLevel2;
     [SerializeField] private Button buttonLevel3;
+    [SerializeField] private Button buttonLevel4;
+
+    [SerializeField] private CanvasGroup panelSettings;
+    [SerializeField] private Button buttonBackToMainFromSettings;
 
     [SerializeField] private string[] levelNames;
     // Start is called before the first frame update
@@ -22,14 +28,21 @@ public class UIMenu : MonoBehaviour
     {
         panelMain.ShowCanvasGroup();
         panelLevelSelection.HideCanvasGroup();
+        panelSettings.HideCanvasGroup();
         
         buttonLevelSelection.onClick.AddListener(ShowLevelSelection);
+        buttonSettings.onClick.AddListener(ShowSettings);
+        
         buttonNewGame.onClick.AddListener(LoadLevel1);
         buttonBackToMain.onClick.AddListener(ShowMainPanel);
+        buttonBackToMainFromSettings.onClick.AddListener(ShowMainPanel);
         
         buttonLevel1.onClick.AddListener(LoadLevel1);
         buttonLevel2.onClick.AddListener(LoadLevel2);
         buttonLevel3.onClick.AddListener(LoadLevel3);
+        buttonLevel4.onClick.AddListener(LoadLevel4);
+        
+        exitButton.onClick.AddListener(ExitGame);
 
         buttonLevel2.interactable = false;
         if (PlayerPrefs.HasKey(levelNames[1]))
@@ -48,19 +61,43 @@ public class UIMenu : MonoBehaviour
                 buttonLevel3.interactable = true;
             }
         }
+        
+        buttonLevel4.interactable = false;
+        if (PlayerPrefs.HasKey(levelNames[3]))
+        {
+            if (PlayerPrefs.GetInt(levelNames[3]) == 1)
+            {
+                buttonLevel4.interactable = true;
+            }
+        }
     }
 
     void ShowLevelSelection()
     {
         panelMain.HideCanvasGroup();
         panelLevelSelection.ShowCanvasGroup();
+        panelSettings.HideCanvasGroup();
+    }
+    
+    void ShowSettings()
+    {
+        panelMain.HideCanvasGroup();
+        panelLevelSelection.HideCanvasGroup();
+        panelSettings.ShowCanvasGroup();
     }
 
     void ShowMainPanel()
     {
         panelMain.ShowCanvasGroup();
         panelLevelSelection.HideCanvasGroup();
+        panelSettings.HideCanvasGroup();
     }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+    
 
     void LoadLevel1()
     {
@@ -75,5 +112,10 @@ public class UIMenu : MonoBehaviour
     void LoadLevel3()
     {
         SceneManager.LoadScene(levelNames[2]);
+    }
+    
+    void LoadLevel4()
+    {
+        SceneManager.LoadScene(levelNames[3]);
     }
 }
